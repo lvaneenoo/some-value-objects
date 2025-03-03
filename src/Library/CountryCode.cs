85a16cb0b997b6c9c0f36c8d67dac511155ebd41
Lorenzo Van Eenoo <lvaneenoo@proton.me>
2025-03-03 16:03:23 +0100

    Add Library project

---
+++ src/Library/CountryCode.cs
@@ -0,0 +1,60 @@
+using System.Diagnostics.CodeAnalysis;
+using System.Text.RegularExpressions;
+
+namespace Common;
+
+public readonly struct CountryCode : IComparable<CountryCode>, IEquatable<CountryCode>
+{
+    public static readonly CountryCode Unknown;
+
+    private readonly string? _value;
+
+    private CountryCode(string value)
+    {
+        _value = value;
+    }
+
+    public static CountryCode Parse(string s)
+    {
+        if (Regex.IsMatch(s, "[A-Z]{2}"))
+        {
+            return new CountryCode(s);
+        }
+
+        throw new FormatException();
+    }
+
+    public static bool TryParse(string s, out CountryCode result)
+    {
+        if (Regex.IsMatch(s, "[A-Z]{2}"))
+        {
+            result = new CountryCode(s);
+            return true;
+        }
+
+        result = Unknown;
+        return false;
+    }
+
+    public int CompareTo(CountryCode other)
+    {
+        if (other._value == null)
+        {
+            return 1;
+        }
+
+        return _value == null ? -1 : _value.CompareTo(other._value);
+    }
+
+    public bool Equals(CountryCode other) => _value == other._value;
+    public override bool Equals([NotNullWhen(true)] object? obj) => obj is CountryCode other && Equals(other);
+    public override int GetHashCode() => _value == null ? 0 : _value.GetHashCode();
+    public override string ToString() => _value ?? "Unknown";
+
+    public static bool operator ==(CountryCode a, CountryCode b) => a.Equals(b);
+    public static bool operator !=(CountryCode a, CountryCode b) => !(a == b);
+    public static bool operator <(CountryCode a, CountryCode b) => a.CompareTo(b) < 0;
+    public static bool operator >(CountryCode a, CountryCode b) => a.CompareTo(b) > 0;
+    public static bool operator <=(CountryCode a, CountryCode b) => a.CompareTo(b) <= 0;
+    public static bool operator >=(CountryCode a, CountryCode b) => a.CompareTo(b) >= 0;
+}
---
+++ src/Library/Name.cs
@@ -0,0 +1,59 @@
+using System.Diagnostics.CodeAnalysis;
+
+namespace Common;
+
+public readonly struct Name : IComparable<Name>, IEquatable<Name>
+{
+    public static readonly Name Unknown;
+
+    private readonly string? _value;
+
+    private Name(string value)
+    {
+        _value = value;
+    }
+
+    public static Name Parse(string s)
+    {
+        if (s.Trim() == "" || s.Length > 50)
+        {
+            throw new FormatException();
+        }
+
+        return new Name(s);
+    }
+
+    public static bool TryParse(string s, out Name result)
+    {
+        if (s.Trim() == "" || s.Length > 50)
+        {
+            result = Unknown;
+            return false;
+        }
+
+        result = new Name(s);
+        return true;
+    }
+
+    public int CompareTo(Name other)
+    {
+        if (other._value == null)
+        {
+            return 1;
+        }
+
+        return _value == null ? -1 : _value.CompareTo(other._value);
+    }
+
+    public bool Equals(Name other) => _value == other._value;
+    public override bool Equals([NotNullWhen(true)] object? obj) => obj is Name other && Equals(other);
+    public override int GetHashCode() => _value == null ? 0 : _value.GetHashCode();
+    public override string ToString() => _value ?? "Unknown";
+
+    public static bool operator ==(Name a, Name b) => a.Equals(b);
+    public static bool operator !=(Name a, Name b) => !(a == b);
+    public static bool operator <(Name a, Name b) => a.CompareTo(b) < 0;
+    public static bool operator >(Name a, Name b) => a.CompareTo(b) > 0;
+    public static bool operator <=(Name a, Name b) => a.CompareTo(b) <= 0;
+    public static bool operator >=(Name a, Name b) => a.CompareTo(b) >= 0;
+}
